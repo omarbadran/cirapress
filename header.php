f3b4a7fef2d28c54f2483e11082d48d848495d90
Omar Badran <engineer.o.badran@gmail.com>
2020-10-19 18:08:30 +0300

    Add components style

--- header.php
+++ header.php
@@ -13,3 +13,25 @@
 <body <?php body_class(); ?> itemscope itemtype="https://schema.org/WebPage">
     
     <?php wp_body_open(); ?>
+
+    <header class="nav-wrap bg-dark fixed-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark px-lg-0">
+                <a class="navbar-brand" href="#">MarketSpot</a>
+
+                <div class="collapse navbar-collapse justify-content-between">
+                    <ul class="navbar-nav">
+                        <li class="nav-item swap-link">
+                            <a href="index.html" class="nav-link">Home</a>
+                        </li>
+                    </ul>
+                    
+                    <ul class="navbar-nav">
+                        <li class="nav-item swap-link">
+                            <a class="nav-link no-arrow btn btn-primary swap-link text-white" href="#">Sign up</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
