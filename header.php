9f90dffb51aa7042de9523abb33acf2764000300
Omar Badran <engineer.o.badran@gmail.com>
2020-10-20 15:13:03 +0300

    Basic header setup

--- header.php
+++ header.php
@@ -17,16 +17,22 @@
     <header class="nav-wrap bg-dark fixed-top">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-dark px-lg-0">
-                <a class="navbar-brand" href="#">MarketSpot</a>
+                <a class="navbar-brand" href="#">CiraPress</a>
 
                 <div class="collapse navbar-collapse justify-content-between">
                     <ul class="navbar-nav">
                         <li class="nav-item swap-link">
-                            <a href="index.html" class="nav-link">Home</a>
+                            <a href="#" class="nav-link">Home</a>
                         </li>
                     </ul>
                     
                     <ul class="navbar-nav">
+                        <li class="nav-item swap-link">
+                            <a class="nav-link" href="#">
+                                <i class="las la-user mr-2" style="font-size:20px;"></i>
+                            </a>
+                        </li>
+
                         <li class="nav-item swap-link">
                             <a class="nav-link no-arrow btn btn-primary swap-link text-white" href="#">Sign up</a>
                         </li>
