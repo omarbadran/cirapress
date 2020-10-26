8cbef4b7cac1920523a098e77e1ebbebcefd4a72
Omar Badran <engineer.o.badran@gmail.com>
2020-10-26 20:02:24 +0300

    Secure sync_product_data function

--- inc/setup.php
+++ inc/setup.php
@@ -11,6 +11,10 @@ add_action('wp_ajax_sync_product_data', function () {
     $post = $_POST['post'] ?? null;
     $product = $_POST['product'] ?? null;
 
+    if ( ! current_user_can('edit_posts') ) {
+        die('fuck of');
+    }
+
     $data = sync_product_data($post, $product);
 
     if ( isset($data['error']) ){
